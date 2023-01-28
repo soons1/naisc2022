6b92899b9bb8200bf4432bb0687a43d603154dff
normantan1 <88649714+normantan1@users.noreply.github.com>
2023-01-28 22:58:36 +0800

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-from textblob import TextBlob
-text = TextBlob("Machine learning (ML) is a field of inquiry devoted to understanding and building methods that learn, that is, methods that leverage data to improve performance on some set of tasks. It is seen as a part of artificial intelligence. Machine learning algorithms build a model based on sample data, known as training data, in order to make predictions or decisions without being explicitly programmed to do so. Machine learning algorithms are used in a wide variety of applications, such as in medicine, email filtering, speech recognition, and computer vision, where it is difficult or unfeasible to develop conventional algorithms to perform the needed tasks.")
-for np in text.noun_phrases:
-    print(np)
