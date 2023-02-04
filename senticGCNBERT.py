813d1ee4bdb3189bc6413784b94b3e10b9d4eb32
slimzaza <106515785+slimzaza@users.noreply.github.com>
2023-02-04 22:48:12 +0800

    Update senticGCNBERT.py

--- senticGCNBERT.py
+++ senticGCNBERT.py
@@ -38,39 +38,3 @@ processed_inputs, processed_indices = preprocessor(main.inputs)
 raw_outputs = model(processed_indices)
 
 post_outputs = postprocessor(processed_inputs=processed_inputs, model_outputs=raw_outputs)
-
-print(post_outputs[0])
-# {'sentence': ['To', 'sum', 'it', 'up', ':', 'service', 'varies', 'from', 'good', 'to', 'mediorce', ',',
-#               'depending', 'on', 'which', 'waiter', 'you', 'get', ';', 'generally', 'it', 'is', 'just',
-#               'average', 'ok', '.'],
-#  'aspects': [[5]],
-#  'labels': [0]}
-
-print(post_outputs[1])
-# {'sentence': ['Everything', 'is', 'always', 'cooked', 'to', 'perfection', ',', 'the', 'service',
-#               'is', 'excellent,', 'the', 'decor', 'cool', 'and', 'understated.'],
-#  'aspects': [[8], [12]],
-#  'labels': [1, 1]}
-
-print(post_outputs[2])
-# {'sentence': ['the', 'only', 'chicken', 'i', 'moderately', 'enjoyed', 'was', 'their', 'grilled',
-#               'chicken', 'special', 'with', 'edamame', 'puree', '.'],
-#  'aspects': [[8, 9], [2], [9]],
-#  'labels': [1, 1, 1]}
-
-'''
-inputs = [
-    {  # Single word aspect
-        "aspects": ["service"],
-        "sentence": "To sum it up : service varies from good to mediorce , depending on which waiter you get ; generally it is just average ok .",
-    },
-    {  # Single-word, multiple aspects
-        "aspects": ["service", "decor"],
-        "sentence": "Everything is always cooked to perfection , the service is excellent, the decor cool and understated.",
-    },
-    {  # Multi-word aspect
-        "aspects": ["grilled chicken", "chicken"],
-        "sentence": "the only chicken i moderately enjoyed was their grilled chicken special with edamame puree .",
-    },
-]
-'''
